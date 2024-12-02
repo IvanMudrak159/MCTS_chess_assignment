81166247e5e20c7c9bc6ebc1c8ecca5b6526bf69
IvanMudrak <46427131+IvanMudrak159@users.noreply.github.com>
2024-12-02 20:49:01 +0100

    implemented node

--- Chess-AI-main/Assets/Scripts/Core/AI/MCTSNode.cs
+++ Chess-AI-main/Assets/Scripts/Core/AI/MCTSNode.cs
@@ -1,11 +1,43 @@
 ﻿namespace Chess
 {
-    using System;
     using System.Collections.Generic;
-    using UnityEngine;
 
     public class MCTSNode
     {
+        public Board GameState { get; private set; }
+        public MCTSNode Parent { get; private set; }
+        public List<MCTSNode> Children { get; private set; }
+        public Move Move { get; private set; }
+        public int VisitCount { get; private set; }
+        public float TotalValue { get; private set; }
+        public List<Move> UnexploredMoves { get; private set; }
+        public bool IsPlayerMove { get; private set; }
 
+        public MCTSNode(Board gameState, MCTSNode parent, Move move, bool isPlayerMove)
+        {
+            MoveGenerator moveGenerator = new MoveGenerator ();
+            GameState = gameState;
+            Parent = parent;
+            Move = move;
+            IsPlayerMove = isPlayerMove;
+            Children = new List<MCTSNode>();
+            UnexploredMoves = moveGenerator.GenerateMoves_DO_NOT_USE(gameState);
+            VisitCount = 0;
+            TotalValue = 0;
+        }
+
+        public MCTSNode AddChild(Move move, Board newGameState)
+        {
+            var childNode = new MCTSNode(newGameState, this, move, IsPlayerMove);
+            Children.Add(childNode);
+            UnexploredMoves.Remove(move);
+            return childNode;
+        }
+
+        public void UpdateStatistics(float simulationResult)
+        {
+            VisitCount++;
+            TotalValue += simulationResult;
+        }
     }
 }
